84b71749a07694f32a56fca8db3abcad0885b872
annamonso <95723084+annamonso@users.noreply.github.com>
2022-10-05 08:28:21 +0200

    Add files via upload

--- puzzle1.py
+++ puzzle1.py
@@ -1,15 +1,19 @@
-from py532lib.i2c import*
+from py532lib .i2c import*
 from py532lib.frame import*
 from py532lib.constants import*
 
-pn532=Pn532_i2c()
-pn532.SAMconfigure();
-
 class RFID:
-    card_data=pn532.read_mifare().get_data()
-    card_info=int.from_bytes(card_data,byteorder='big',signed=True)
-    card_hexa=hex(card_info)
-    card=''
-    for i in range(16,len(card_hexa)):
-        card=card+str(card_hexa[i])
-    print(card.upper())
+        def main(self):
+                pn532=Pn532_i2c()
+                pn532.SAMconfigure();
+                card_data= pn532.read_mifare().get_data()
+#passar de bytearray a hexadecimal
+                card_info=int.from_bytes(card_data,byteorder='big', signed=True)
+                card_hexa= hex(card_info)
+                card=''
+                for i in range(16,len(card_hexa)):
+                        card=card+str(card_hexa[i])
+                print (card.upper())
+if __name__=='__main__':
+        pn532=RFID()
+        pn532.main()
